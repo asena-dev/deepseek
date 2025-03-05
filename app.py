da6739fe5824c3742bd8cf346273890225301924
Adenilson Sena <asena.developer@outlook.com>
2025-03-05 16:05:26 -0300

    Envio do projeto

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+import streamlit as st
+from langchain_groq import ChatGroq
+
+print("GROQ_API_KEY:", os.getenv("GROQ_API_KEY"))
+
+# Agora a chave de API é carregada corretamente do .env
+llm = ChatGroq(
+    model="deepseek-r1-distill-llama-70b",
+    api_key=os.getenv("GROQ_API_KEY")  # <-- Aqui está o ajuste!
+)
+
+st.set_page_config(page_title="Chat Deep", layout="centered")
+st.title("Teste com DeepSeek")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+    
+messages = st.session_state["messages"]
+for type, content in messages:
+    chat = st.chat_message(type)
+    chat.markdown(content)
+
+in_message = st.chat_input("Envie sua dúvida")
+if in_message:
+    messages.append(("human", in_message))
+    chat = st.chat_message("human")
+    chat.markdown(in_message)
+    
+    response = llm.invoke(messages).content
+    messages.append(("ai", response))
+    
+    chat = st.chat_message("ai")
+    chat.markdown(response)
